7fcdb7450e5b0700e9de708db55dda3e86bc3eef
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-11 16:17:55 -0500

    Create test for add_piece.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Board do
   it 'can create an empty board' do
     board = Board.new
 
-    expect(board.build_board).to eq(["ABCDEFG", "......", "......", "......", "......", "......", "......."])
+    expect(board.build_board).to eq(["ABCDEFG", ".......", ".......", ".......", ".......", ".......", "......."])
   end
+
+  it 'can add a piece based on provided input' do
+    board = Board.new
+    board.build_board
+
+    board.add_piece(:player, "a")
+
+    expect(board.board).to eq(["ABCDEFG", ".......", ".......", ".......", ".......", ".......", "X......"])
+  end
+
+
 end
