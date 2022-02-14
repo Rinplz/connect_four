a9ecc800486e2713b27b52d3ca07d33710ee17f7
Rin Fabian <pfabianschool@gmail.com>
2022-02-14 01:07:45 -0600

    test; created a test for deciding if the game is at a draw. feat; added method is_draw? to check if game is currently at a draw state

--- lib/board.rb
+++ lib/board.rb
@@ -23,19 +23,27 @@ attr_reader :board, :column_count, :invalid_columns
       piece = "O"
     end
     column = column - 1
+
     @invalid_columns.each do |index|
       if column == index || column < 0 || column > 6
         return "Please input a valid column"
       end
     end
+
     row = column_count[column]
     arr_to_change = @board[row].split("")
     arr_to_change[column] = piece
     @board[row] = arr_to_change.join("")
     column_count[column] -= 1
+
     if column_count[column] == 0
       @invalid_columns << column
     end
+
     return board
+  end
+
+  def is_draw?
+    return @invalid_columns.length == 7
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,5 +28,19 @@ RSpec.describe Board do
     expect(board.add_piece(:human, 1)).to eq("Please input a valid column")
   end
 
+  it 'can detect a draw state' do
+    board = Board.new
+    board.build_board
+
+    7.times do |index|
+      6.times do
+        board.add_piece(:human, (index + 1))
+      end
+    end
+
+    expect(board.invalid_columns).to eq([0, 1, 2, 3, 4, 5, 6])
+    expect(board.is_draw?).to eq(true)
+  end
+
 
 end
