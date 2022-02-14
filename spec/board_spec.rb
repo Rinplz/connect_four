dfdd0708275f84f933f6a215bb331bb888342ab3
Rin Fabian <pfabianschool@gmail.com>
2022-02-14 16:27:02 -0600

    tests; added tests for all end of game states. feat; added the end of game method that checks for all types of win states

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 require './lib/board'
+require 'pry'
 
 RSpec.describe Board do
   it 'can create an empty board' do
@@ -38,8 +39,74 @@ RSpec.describe Board do
       end
     end
 
+    board.is_draw?
+
     expect(board.invalid_columns).to eq([0, 1, 2, 3, 4, 5, 6])
-    expect(board.is_draw?).to eq(true)
+    expect(board.game_result).to eq("Draw")
+  end
+
+  it 'can detect if game is won horizontally' do
+    board = Board.new
+    board.build_board
+
+    board.add_piece(:human, 1)
+    board.add_piece(:human, 2)
+    board.add_piece(:human, 3)
+    board.add_piece(:human, 4)
+    board.end_of_game
+
+    expect(board.game_result).to eq("Player Wins")
+  end
+
+  it 'can detect if a game is won vertically' do
+    board = Board.new
+    board.build_board
+
+    board.add_piece(:human, 1)
+    board.add_piece(:human, 1)
+    board.add_piece(:human, 1)
+    board.add_piece(:human, 1)
+    board.end_of_game
+
+    expect(board.game_result).to eq("Player Wins")
+  end
+
+  it 'can detect if a game is won diagonally from bottom left to top right' do
+    board = Board.new
+    board.build_board
+
+    board.add_piece(:human, 1)
+    board.add_piece(:computer, 2)
+    board.add_piece(:human, 2)
+    board.add_piece(:computer, 3)
+    board.add_piece(:computer, 3)
+    board.add_piece(:human, 3)
+    board.add_piece(:computer, 4)
+    board.add_piece(:computer, 4)
+    board.add_piece(:computer, 4)
+    board.add_piece(:human, 4)
+    board.end_of_game
+
+    expect(board.game_result).to eq("Player Wins")
+  end
+
+  it 'can detect if a game is won diagonally from bottom right to top left' do
+    board = Board.new
+    board.build_board
+
+    board.add_piece(:human, 7)
+    board.add_piece(:computer, 6)
+    board.add_piece(:human, 6)
+    board.add_piece(:computer, 5)
+    board.add_piece(:computer, 5)
+    board.add_piece(:human, 5)
+    board.add_piece(:computer, 4)
+    board.add_piece(:computer, 4)
+    board.add_piece(:computer, 4)
+    board.add_piece(:human, 4)
+    board.end_of_game
+
+    expect(board.game_result).to eq("Player Wins")
   end
 
 
